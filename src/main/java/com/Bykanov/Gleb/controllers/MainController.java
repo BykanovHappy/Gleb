7a25412fdc35581349bc1e1a9bf98e2f2bd56c8e
Gleb <bykanovgleb123@gmail.com>
2023-11-14 21:17:12 +0300

    Быканов Лаб 2 БД Добавление базы данных

--- src/main/java/com/Bykanov/Gleb/controllers/MainController.java
+++ src/main/java/com/Bykanov/Gleb/controllers/MainController.java
@@ -1,14 +1,19 @@
 package com.Bykanov.Gleb.controllers;
 
 
+import com.Bykanov.Gleb.domain.entity.Student;
+import com.Bykanov.Gleb.domain.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
+    @Autowired
+    private StudentRepo studentRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",
@@ -16,6 +21,35 @@ public class MainController {
     {
         model.addAttribute("name", name);
         return "greeting";
+    }
+    @GetMapping("/")
+    public String main (Model model)
+    {
+        Iterable<Student> students = studentRepo.findAll();
+        model.addAttribute("students", students);
+        return "main";
+    }
+
+    @PostMapping("/")
+    public String add(@RequestParam String text, @RequestParam Integer groupp, Model model){
+        final Student student = new Student(text, groupp);
+        studentRepo.save(student);
+        Iterable<Student> students = studentRepo.findAll();
+        model.addAttribute("students", students);
+        return "main";
+    }
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model){
+        Iterable<Student> students;
+        if (filter != null && !filter.isEmpty()){
+            int flrt = Integer.parseInt(filter);
+            students = studentRepo.findByGroupp(flrt);
+        }
+        else {
+            students = studentRepo.findAll();
+        }
+        model.addAttribute("students", students);
+        return "main";
     }
 
 }
---
+++ src/main/java/com/Bykanov/Gleb/domain/repo/StudentRepo.java
@@ -0,0 +1,12 @@
+package com.Bykanov.Gleb.domain.repo;
+
+import com.Bykanov.Gleb.domain.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StudentRepo extends CrudRepository<Student, Long> {
+
+    List<Student> findByGroupp(int groupp);
+
+}
