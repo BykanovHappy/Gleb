cce4d4927d392a34170192213b66e6b65d78bd22
Gleb <bykanovgleb123@gmail.com>
2023-11-14 11:42:37 +0300

    Быканов Лаб 1 БД

---
+++ src/main/java/com/Bykanov/Gleb/GlebApplication.java
@@ -0,0 +1,13 @@
+package com.Bykanov.Gleb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bykanov/Gleb/GlebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bykanov.Gleb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
